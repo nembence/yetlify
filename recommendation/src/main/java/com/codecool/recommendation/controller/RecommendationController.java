8f82cff18928739629d6d2271b22d12b83fef434
Bence Nemeth <57453541+nembence@users.noreply.github.com>
2020-11-06 12:22:37 +0100

    create Recommendation controller

---
+++ recommendation/src/main/java/com/codecool/recommendation/controller/RecommendationController.java
@@ -0,0 +1,26 @@
+package com.codecool.recommendation.controller;
+
+import com.codecool.recommendation.model.Recommendation;
+import com.codecool.recommendation.service.RecommendationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/recommendation")
+public class RecommendationController {
+
+    @Autowired
+    private RecommendationService service;
+
+    @GetMapping("/get-all")
+    public List<Recommendation> getAllRecommendations() {
+        return service.getAllRecommendation();
+    }
+
+    @PostMapping("/new")
+    public void saveRecommendation(@RequestBody Recommendation recommendation) {
+        service.saveRecommendation(recommendation);
+    }
+}
