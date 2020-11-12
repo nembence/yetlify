f0f1b2aa8c5b291854cfc842613b7e609812cb01
Bence Nemeth <57453541+nembence@users.noreply.github.com>
2020-11-12 15:07:54 +0100

    Change App component

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -3,28 +3,31 @@ import Header from "../Header/Header";
 import Main from "../Main/Main";
 import VideoDetail from "../VideoDetail/VideoDetail";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { VideoProvider } from "../../context/VideoContext";
 
 const App = () => {
     const containerStyle = {
         position: "absolute",
-        left: "28%",
+        left: "25%",
         margin: "auto",
         width: "50%",
         top: "100px",
     };
 
     return (
-        <Router>
-            <Header />
-            <div style={containerStyle}>
-                <Switch>
-                    <Route exact path="/">
-                        <Main />
-                    </Route>
-                    <Route path="/video/:id" component={VideoDetail} />
-                </Switch>
-            </div>
-        </Router>
+        <VideoProvider>
+            <Router>
+                <Header />
+                <div style={containerStyle}>
+                    <Switch>
+                        <Route exact path="/">
+                            <Main />
+                        </Route>
+                        <Route path="/video/:id" component={VideoDetail} />
+                    </Switch>
+                </div>
+            </Router>
+        </VideoProvider>
     );
 };
 
